e3146516eef91ba8522ed7b0e78aa669b86b04a1
seemsartless <david.s.toronto@gmail.com>
2023-12-08 11:31:40 -0500

    Hello world

---
+++ client_1_read_details.py
@@ -0,0 +1,9 @@
+
+
+# Simple Mastodon client Python script to:
+#   1. Read local configuration and API key
+#   2. Connect to Mastodon user
+#   3. Read and print some details
+#
+
+print("Hello world.")
